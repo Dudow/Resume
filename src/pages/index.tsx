6fea3d315a73cdea3f220dbda130ecfce0d3ed5d
Dudow <edu30.nm@gmail.com>
2022-03-23 13:29:18 -0300

    fix: fix resize behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
-import { AboutMe } from "./Content/AboutMe";
-import { Introduction } from "./Content/Introduction";
-import { Projects } from "./Content/Projects";
+import AboutMe from "./Content/AboutMe";
+import Introduction from "./Content/Introduction";
+import Projects from "./Content/Projects";
 
 function jump(h: string) {
-  var url = location.href; //Save down the URL without hash.
-  history.replaceState(null, "", url); //Don't like hashes. Changing it back.
+  history.replaceState(null, "", h);
 }
+
 const Home: NextPage = () => {
   useEffect(() => {
     window.addEventListener("resize", function () {
